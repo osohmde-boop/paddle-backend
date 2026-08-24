0a653b986444f40b8e7f7bf6eabf99f83119e3e7
osohmde-boop <osohmde@gmail.com>
2026-08-24 18:45:42 +0300

    Update shopier.js

--- api/shopier.js
+++ api/shopier.js
@@ -34,7 +34,8 @@ export default async function handler(req, res) {
         media: [
           {
             url: 'https://i.ibb.co/YT1RPZdx/image.png',
-            placement: 'cover'
+            placement: 'cover',
+            type: 'image'
           }
         ]
       })
